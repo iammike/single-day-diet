f591f2a8bba6bd3d0d97286acd2b8cf2fc0a29d1
Michael Collins <michael.collins@moovel.com>
2019-08-23 07:01:10 -0700

    Initial Commit

---
+++ Single Day Diet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Single Day Diet
+//
+//  Created by Michael Collins on 8/23/19.
+//  Copyright © 2019 Michael Collins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
