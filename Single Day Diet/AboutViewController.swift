7f46283fe8b141f0d1c94d5b1e706e3d89dd2550
Michael Collins <michael.collins@moovel.com>
2019-09-11 11:53:29 -0700

    Adding About screen and app rename

---
+++ Single Day Diet/AboutViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AboutViewController.swift
+//  Single Day Diet
+//
+//  Created by Michael Collins on 9/11/19.
+//  Copyright © 2019 Michael Collins. All rights reserved.
+//
+
+import Foundation
---
+++ Single Day Diet/LeftToRightSegue.swift
@@ -0,0 +1,9 @@
+//
+//  LeftToRightSegue.swift
+//  Single Day Diet
+//
+//  Created by Michael Collins on 9/11/19.
+//  Copyright © 2019 Michael Collins. All rights reserved.
+//
+
+import Foundation
