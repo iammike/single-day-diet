b0a542489bbd17fd3cfb6d114e3fedf30470af1c
Michael Collins <michael.collins@moovel.com>
2019-09-12 09:37:02 -0700

    Last commit didn`t contain all the files hrmmm

--- Single Day Diet/AboutViewController.swift
+++ Single Day Diet/AboutViewController.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2019 Michael Collins. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var buyBook: UIButton!
+    @IBOutlet weak var visitSite: UIButton!
+    @IBOutlet weak var contactMe: UIButton!
+    
+    @IBAction func linkPressed(sender: UIButton) {
+        var targetURL = ""
+        
+        switch sender {
+        case buyBook:
+            targetURL = "https://amzn.to/2NaoxmW"
+        case visitSite:
+            targetURL = "http://www.iammike.org"
+        case contactMe:
+            targetURL = "mailto:mike@iammike.org"
+        default:
+            break
+        }
+        
+        UIApplication.shared.open(URL(string: targetURL)!)
+    }
+}
